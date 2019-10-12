51d70c53955c8dc9888e0ef969f4c45ae2de0715
Vlad <v.rozanoff@gmail.com>
2019-10-12 14:10:14 +0300

    Start implementing Bridge

---
+++ Patterns/Bridge/IBuildingCompany.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bridge
+{
+    interface IBuildingCompany
+    {
+        void BuildFoundation();
+        void BuildRoom();
+        void BuildRoof();
+        
+    }
+}
---
+++ Patterns/Bridge/IWallCreator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bridge
+{
+    interface IWallCreator
+    {
+        void CreateWall();
+    }
+}
