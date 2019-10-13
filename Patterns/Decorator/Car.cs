8f32854c9318ad08fd1c26ada07521909480c6f9
Vlad <v.rozanoff@gmail.com>
2019-10-13 21:32:18 +0300

    Start implementation of Decorator pattern

---
+++ Patterns/Decorator/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decorator
+{
+    class Car
+    {
+        protected String BrandName { get; set; }
+        public virtual void Go()
+        {
+            Console.WriteLine("I'm" + BrandName + " and I'm on my way...");
+        }
+    }
+}
---
+++ Patterns/Decorator/Mercedes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decorator
+{
+    class Mercedes : Car
+    {
+        public Mercedes()
+        {
+            BrandName = "Mercedes";
+        }
+    }
+}
